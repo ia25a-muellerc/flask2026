071ad414039893d8f85b70d4b2aa4141eea548b2
besa ramadani <ramadanib@bzz.ch>
2026-02-02 14:12:17 +0100

    fertig

--- app/app.py
+++ app/app.py
@@ -59,6 +59,10 @@ def about() -> str:
 def warenkorb() -> str:
     return render_template("warenkorb.html", languages=languages)
 
+@app.route("/data")
+def data() -> str:
+    return render_template("data.html", languages=languages)
+
 @app.route("/submit", methods=["POST"])
 def submit():
     app.logger.info("Form submitted")
